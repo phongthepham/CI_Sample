f25efe13319b5666da872171b8f4766f5232ebfc
phongthepham <phamthephong83@gmail.com>
2022-10-09 13:25:05 +0700

    Initial commit

---
+++ src/test/java/FirstTestCase.java
@@ -0,0 +1,12 @@
+import org.testng.annotations.*;
+
+public class FirstTestCase extends SeleniumAbstractTest {
+    @Test
+    public void firstTC_TestCase_01() {
+        System.out.println("firstTC_TestCase_01");
+    }
+    @Test
+    public void firstTC_TestCase_02() {
+        System.out.println("firstTC_TestCase_02");
+    }
+}
---
+++ src/test/java/SecondTestCase.java
@@ -0,0 +1,13 @@
+import org.testng.annotations.Test;
+
+public class SecondTestCase extends SeleniumAbstractTest {
+    @Test
+    public void secondTC_TestCase_01() {
+        System.out.println("secondTC_TestCase_01");
+    }
+    @Test
+    public void secondTC_TestCase_02() {
+        System.out.println("secondTC_TestCase_02");
+    }
+}
+    
